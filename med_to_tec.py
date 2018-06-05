5e9ad1ea6cfc209c113d63325d33424ca634a9b3
Laubach Laboratory <laubachcsn@gmail.com>
2018-06-05 11:38:10 -0400

    Add files via upload
    Python functions to convert Time-Event codes into numpy array

---
+++ med_to_tec.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+MED_to_TEC.py
+
+Converts MedPC file into list of [time,event] codes
+
+Requires the following DISKVARS:
+DIM A=9900, B=3
+
+Requires Time-Event pairs to be saved as:
+TIME.EVENT
+For example, an event 1 at exactly 536 seconds from start will be recorded as 
+268000.001
+Currently, the system clocks at 2ms, so time codes are divided by 500 
+to convert them to seconds.
+Events are converted to integers.
+The final product for this event is 
+[536,1]
+
+
+Created Jan 5 15:23 2015
+Last Edited Feb 8 11:47 2016 
+
+Author: Kyra Swanson
+"""
+import numpy as np
+import re
+
+#convert time to seconds (BTIME at 2ms, 500/sec)
+timeslice = 500
+# timeslice = 200 (old files timeslice at 5ms = 200)
+
+
+
+#convert a .med file into a readable format 
+def cleanLines(item):
+    start = item.index(":") + 1
+    item = item[start:]
+    item = item.replace("\n","")
+    item = item.split()
+    return item
+
+def breakLine(lines):
+    Time_Event = []
+    for item in lines:
+        for el in item:
+            temp = el.split(".")
+            if temp[0] != "0":
+                temp[0] = float(temp[0])/timeslice
+                temp[1] = float(temp[1])
+                Time_Event.append(temp)
+    return np.asarray(Time_Event)    
+    
+def convert(fileName,variable = "A"):
+    fid = open(fileName) #open file
+    lines = fid.readlines() #store lines in list
+    fid.close()
+    
+    flag = variable + ":\n" #find variable flag (eg A:)
+    start_index = next(i for i,x in enumerate(lines) if x == flag) + 1 
+    lines = lines[start_index:]
+    stop_index = 0
+    
+    while not (re.search('[a-zA-Z]', lines[stop_index])): #stops at the next variable or comment
+        lines[stop_index] = cleanLines(lines[stop_index]) #clean lines
+        stop_index +=  1
+    
+    lines = lines[:stop_index]
+    TEC = breakLine(lines) #break lines, store time,event in list Time_Event
+    return TEC
+    
+
+
+#****************************************************************************    
+#   PULLING OUT DATA    
+    
+#returns times for a given code    
+def findTimes(EV,code):
+    return [val[0] for (i,val) in enumerate(EV) if val[1] == code] 
+    
+#Returns the value for a given variable
+def findVariable(fileName,variable):
+    fid = open(fileName)
+    lines = fid.readlines()
+    fid.close()
+    
+    for i,j in enumerate(lines):
+        if j[0] == variable:
+            answer = lines[i]
+    
+    answer = answer[:-5]
+    start = re.search("\d", answer).start()
+    return int(answer[start:])    
+    
+
+
+
+
+
